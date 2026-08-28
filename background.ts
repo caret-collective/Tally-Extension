f4586d0c35e6ed4bc8b05c42a2ce0dc021f11eff
John Goodliff <9030780+twocaretcat@users.noreply.github.com>
2026-08-27 22:30:06 -0600

    refactor: rewrite source in TypeScript. Also fixes #2

--- background.ts
+++ background.ts
@@ -1,108 +1,117 @@
-;(function(){
-	function count(info) {
-		const text = info.selectionText,
-					len = text.length
+type CountResult = {
+	characters: number;
+	words: number;
+	sentences: number;
+	paragraphs: number;
+	spaces: number;
+	letters: number;
+	digits: number;
+	specialcharacters: number;
+};
 
-		let	wordflag = false,
-				sentflag = false,
-				paraflag = false
-				count = {
-					characters: 0,
-					words: 0,
-					sentences: 0,
-					paragraphs: 0,
-					spaces: 0,
-					letters: 0,
-					digits: 0,
-					specialcharacters: 0
+function count(info: chrome.contextMenus.OnClickData): void {
+	const text = info.selectionText!;
+	const len = text.length;
+
+	let wordflag = false;
+	let sentflag = false;
+	let paraflag = false;
+
+	const count: CountResult = {
+		characters: 0,
+		words: 0,
+		sentences: 0,
+		paragraphs: 0,
+		spaces: 0,
+		letters: 0,
+		digits: 0,
+		specialcharacters: 0
+	};
+
+	for (let i = 0; i < len; i++) {
+		const current = text[i]!;
+
+		count.characters++;
+
+		if (/\d/.test(current)) {
+			wordflag = true;
+			sentflag = true;
+			paraflag = true;
+			count.digits++;
+		}
+		else if (/\w/.test(current)) {
+			wordflag = true;
+			sentflag = true;
+			paraflag = true;
+			count.letters++;
+		}
+		else {
+			if (/ /.test(current)) {
+				count.spaces++;
+
+				if (wordflag) {
+					wordflag = false;
+					count.words++;
+				}
+			}
+			else if (/[\.\?\!]/.test(current)) {
+				if (wordflag) {
+					wordflag = false;
+					count.words++;
 				}
 
-		for (var i = 0; i < len; i++) {
-			current = text[i]
-			count.characters++
-
-			if (/\d/.test(current)) {
-				wordflag = true
-				sentflag = true
-				paraflag = true
-				count.digits++
+				if (sentflag) {
+					sentflag = false;
+					count.sentences++;
+				}
 			}
+			else if (/\n/.test(current)) {
+				if (wordflag) {
+					wordflag = false;
+					count.words++;
+				}
 
-			else if (/\w/.test(current)) {
-				wordflag = true
-				sentflag = true
-				paraflag = true
-				count.letters++
+				if (sentflag) {
+					sentflag = false;
+					count.sentences++;
+				}
+
+				if (paraflag) {
+					paraflag = false;
+					count.paragraphs++;
+				}
 			}
-
 			else {
-				if (/ /.test(current)) {
-					count.spaces++
-
-					if (wordflag) {
-						wordflag = false
-						count.words++
-					}
-				}
-				else if (/[\.\?\!]/.test(current)) {
-					if (wordflag) {
-						wordflag = false
-						count.words++
-					}
-
-					if (sentflag) {
-						sentflag = false
-						count.sentences++
-					}
-				}
-				else if (/\n/.test(current)) {
-					if (wordflag) {
-						wordflag = false
-						count.words++
-					}
-
-					if (sentflag) {
-						sentflag = false
-						count.sentences++
-					}
-
-					if (paraflag) {
-						paraflag = false
-						count.paragraphs++
-					}
-				}
-				else {
-					count.specialcharacters++
-				}
+				count.specialcharacters++;
 			}
 		}
-
-		if (wordflag) {
-			count.words++
-		}
-
-		if (sentflag) {
-			count.sentences++
-		}
-
-		if (paraflag) {
-			count.paragraphs++
-		}
-
-		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-			chrome.tabs.sendMessage(tabs[0].id, count)
-		});
 	}
 
-	chrome.contextMenus.create({
-		id: 'twocaretcat-Tally-count',
-		title: 'Tally Word Counter: Count',
-		contexts: ['selection']
-	});
+	if (wordflag) {
+		count.words++;
+	}
 
-	chrome.contextMenus.onClicked.addListener(function(info, tab) {
-		if (info.menuItemId == 'twocaretcat-Tally-count') {
-			count(info)
-		}
+	if (sentflag) {
+		count.sentences++;
+	}
+
+	if (paraflag) {
+		count.paragraphs++;
+	}
+
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.sendMessage(tabs[0]!.id!, count);
 	});
-})();
+}
+
+chrome.contextMenus.create({
+	id: 'twocaretcat-Tally-count',
+	title: 'Tally Word Counter: Count',
+	contexts: ['selection']
+});
+
+chrome.contextMenus.onClicked.addListener(function(info) {
+	if (info.menuItemId == 'twocaretcat-Tally-count') {
+		count(info);
+	}
+});
