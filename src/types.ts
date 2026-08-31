99f286401b54350a42396aa8c431d396ce9cf00f
John Goodliff <9030780+twocaretcat@users.noreply.github.com>
2026-08-31 17:05:31 -0600

    feat: add types.ts

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export type CountResult = {
+	characters: number;
+	words: number;
+	sentences: number;
+	paragraphs: number;
+	lines: number;
+	spaces: number;
+	letters: number;
+	digits: number;
+	punctuation: number;
+	symbols: number;
+};
